b9bec804e7a510c03d8059b0f7ac44556d77b7d1
RyutoShimada <business.sub10110@gmail.com>
2021-05-18 11:24:05 +0900

    ゲーム開始に必要な人数に達すると、スタートボタンが出てきて、押すとゲームがスタートする

--- Assets/Scripts/shimada/TagGameManager.cs
+++ Assets/Scripts/shimada/TagGameManager.cs
@@ -13,31 +13,49 @@ public class TagGameManager : MonoBehaviour
     [SerializeField] Text m_console = null;
     /// <summary>ゲームが始まっているかを管理するフラグ</summary>
     bool m_isGameStarted = false;
+    [SerializeField] int m_maxPlayerCount = 4;
     [SerializeField] int m_startPlayerCount = 1;
+    [SerializeField] Button m_startButton = null;
 
     void Start()
     {
         m_console.text = "Wait for other players...";
+        m_startButton.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        // 入室していて、まだゲームが始まっていない状態で、人数が揃った時
-        if (PhotonNetwork.InRoom && !m_isGameStarted
-            && PhotonNetwork.CurrentRoom.PlayerCount >= m_startPlayerCount)
-        {
-            // ゲームを開始する
-            m_console.text = "Game Start!";
-            Invoke("ClearConsole", 1.5f);   // 1.5秒後に表示を消す
-            m_isGameStarted = true; // ゲーム開始フラグを立てる
+        if (!PhotonNetwork.InRoom) return;
 
-            // マスタークライアントにより、ランダムに鬼を決める
-            if (PhotonNetwork.IsMasterClient)
+        if (!m_isGameStarted)
+        {
+            m_console.text = $"{PhotonNetwork.CurrentRoom.PlayerCount} / {m_maxPlayerCount}"; //部屋の最大人数と現在の人数を表示
+                                                                                              // 入室していて、まだゲームが始まっていない状態で、人数が揃った時
+            if (PhotonNetwork.CurrentRoom.PlayerCount >= m_startPlayerCount)
             {
-                PlayerController2D[] players = GameObject.FindObjectsOfType<PlayerController2D>();
-                PhotonView view = players[Random.Range(0, players.Length)].GetComponent<PhotonView>();
-                view.RPC("Tag", RpcTarget.All);
+                m_startButton.gameObject.SetActive(true);//STARTボタンを有効にする
             }
+        }
+    }
+
+    /// <summary>
+    /// ボタンから呼ばれる
+    /// </summary>
+    public void PlayStart()
+    {
+        m_startButton.gameObject.SetActive(false);//STARTボタンを無効にする
+
+        // ゲームを開始する
+        m_console.text = "Game Start!";
+        Invoke("ClearConsole", 1.5f);   // 1.5秒後に表示を消す
+        m_isGameStarted = true; // ゲーム開始フラグを立てる
+
+        // マスタークライアントにより、ランダムに鬼を決める
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PlayerController2D[] players = GameObject.FindObjectsOfType<PlayerController2D>();
+            PhotonView view = players[Random.Range(0, players.Length)].GetComponent<PhotonView>();
+            view.RPC("Tag", RpcTarget.All);
         }
     }
 
