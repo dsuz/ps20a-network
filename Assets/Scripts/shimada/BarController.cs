b0e686b2470ceeb73baa33ccf2842db222d3e581
RyutoShimada <business.sub10110@gmail.com>
2021-05-18 13:22:40 +0900

    回転する壁追加した ゲームスタートを押してもisMineにしていないので両方スタートボタン押さないと始まらないので次回修正する

---
+++ Assets/Scripts/shimada/BarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarController : MonoBehaviour
+{
+    [SerializeField] bool m_rotate = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_rotate)
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 0, 1));
+        }
+        else
+        {
+
+        }
+    }
+}
