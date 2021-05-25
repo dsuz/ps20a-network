f0aeaad17430c0ec6a69954dc01a1931ede89396
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-05-25 11:55:24 +0900

    timemanagerスクリプトを作成した

---
+++ Assets/Scripts/Kasai/TimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 制限時間を管理する
+/// </summary>
+public class TimeManager : MonoBehaviour
+{
+    /// <summary>制限時間 </summary>
+    [SerializeField] int m_limitSecond = 90;
+    /// <summary>ゲーム内の経過時間 </summary>
+    float m_time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_time += Time.deltaTime;
+        m_limitSecond -= (int)m_time;
+    }
+}
