50362913d13ab2d112cf7487b9a7c84daca002ad
RyutoShimada <business.sub10110@gmail.com>
2021-06-29 13:29:48 +0900

    MasterGameSceneで動かせるようにしている最中

---
+++ Assets/Scripts/shimada/RotateBackground.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBackground : MonoBehaviour
+{
+    [SerializeField] float _rotateSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, _rotateSpeed);
+    }
+}
